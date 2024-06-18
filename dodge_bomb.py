bdc548d0680aa90946ae296946f7511f4f3156f5
sunao <c0b23018cb@edu.teu.ac.jp>
2024-06-18 17:28:26 +0900

    演習1途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,73 +1,103 @@
 import os
-import sys
 import random
+import sys
 import pygame as pg
 
+WIDTH, HEIGHT = 1600, 900
+DELTA = {  # 移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
 
-WIDTH, HEIGHT = 1200, 500
-DELTA = {pg.K_UP:(0,-5),
-         pg.K_DOWN:(0,+5),
-         pg.K_LEFT:(-5,0),
-         pg.K_RIGHT:(+5,0),
-         }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
     引数:こうかとんRect,または,爆弾Rect
     戻り値：真理値タプル（横方向，縦方向）
     画面内ならTrue/画面外ならFalse
     """
-    yoko,tate = True,True
-    if rct.left < 0 or WIDTH < rct.right:
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
         tate = False
-    return yoko,tate
+    return yoko, tate
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
+    font = pg.font.Font(None, 80)
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    kk_flip_img = pg.transform.flip(kk_img, True, False)  # 反転画像を作成
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bb_img = pg.Surface((20,20))
-    bb_img.set_colorkey((0,0,0))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_rct = bb_img.get_rect()
-    bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
-    vx,vy = +5,+5
+
+    bb_img = pg.Surface((20, 20))  # 1辺が20の空のSurfaceを作る
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 空のSurfaceに赤い円を描く
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5  # 爆弾の横方向速度、縦方向速度
+
     clock = pg.time.Clock()
     tmr = 0
+    kokaton = kk_img
+    
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
-        if kk_rct.colliderect(bb_rct):
-            return
-        screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):  # 衝突判定
+            return  # ゲームオーバー
+        screen.blit(bg_img, [0, 0])
+        txt = font.render(str(tmr/50), True, (255, 255, 255))
+        screen.blit(txt, [300, 200])
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        for k,v in DELTA.items():
+
+        for k, v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
-        kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True,True):
-            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
-        screen.blit(kk_img, kk_rct)
+        
+        if sum_mv==[-5,0]:
+            kokaton = kk_img
+        elif sum_mv==[-5,-5]:
+            kokaton = pg.transform.rotozoom(kk_img,-45,1)
+        elif sum_mv==[-5, +5]:
+            kokaton = pg.transform.rotozoom(kk_img,45,1)
+        elif sum_mv==[+5, 0]:
+            kokaton = kk_flip_img
+        elif sum_mv==[+5, -5]:
+            kokaton = pg.transform.rotozoom(kk_flip_img,45,1)
+        elif sum_mv==[0, -5]:
+            kokaton = pg.transform.rotozoom(kk_flip_img,90,1)
+        elif sum_mv==[+5, +5]:
+            kokaton = pg.transform.rotozoom(kk_flip_img,-45,1)
+        elif sum_mv==[0, +5]:
+            kokaton = pg.transform.rotozoom(kk_flip_img,-90,1)
+            
 
-        bb_rct.move_ip(vx,vy)
-        yoko,tate = check_bound(bb_rct)
-        if not yoko:
-            vx*= -1
-        if not tate:
-            vy*=-1
-        screen.blit(bb_img,bb_rct)
+        kk_rct.move_ip(sum_mv)
+
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kokaton, kk_rct)
+
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出たら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出たら
+            vy *= -1
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
